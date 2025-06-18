85d72ce2599282cde3525aa56ef017e6e49547ba
arshedgithub <arshedahmedmail@gmail.com>
2025-06-18 06:50:30 +0530

    adding posts

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
@@ -8,7 +9,8 @@ Route::get('/', function () {
 });
 
 Route::post('/register', [UserController::class, 'register']);
-
 Route::post('/login', [UserController::class, 'login']);
-
 Route::post('/logout', [UserController::class, 'logout']);
+
+// post related routes
+Route::post('/create-post', [PostController::class, 'createPost']);

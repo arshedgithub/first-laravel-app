b0f5cce5211cc358d59335cf56eb148ef6cefb7e
arshedgithub <arshedahmedmail@gmail.com>
2025-06-18 07:14:00 +0530

    get post by user

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,11 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
-    $posts = Post::all();
+    $posts = [];
+    if (auth()->check()) {
+        $posts = auth()->user()->usersCoolPosts()->latest()->get();
+    }
+    // $posts = Post::where('user_id', auth()->id())->get();
     return view('home', ['posts' => $posts]);
 });
 
